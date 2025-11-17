0f9ae1d547bc808226f61ce74cb4cb132afaba39
Joshua Akinyemi <joshuacakinyemi@gmail.com>
2025-11-17 10:01:20 -0500

    pushing

--- index.js
+++ index.js
@@ -5,8 +5,38 @@
  */
 
 class Task {
- //write your code here
- 
+    static allTasks = []
+    #completed = false
+    #minutesSpent = 0
+    constructor(title, priority) {
+        this.title = title
+        this.priority = priority
+    }
+    completedStatus() {
+        return this.#completed
+    }
+    timeSpent() {
+        return this.#minutesSpent
+    }
+    workOn(minutes) {
+        this.#minutesSpent += minutes
+        console.log(`Worked on ${this.title} for ${minutes} minutes. Total time: ${this.#minutesSpent} minutes`)
+    }
+    complete() {
+        this.#completed = true
+        console.log(`${this.title} has been completed!`)
+    }
+    isComplete() {
+        return this.#completed
+    }
+    static getTotalTasks() {
+        Task.allTasks.push(title)
+        return this.allTasks
+    }
+    findByTitle(title) {
+        const found = Task.allTasks.find(title)
+        return found
+    }
 }
 
 //Test class with examples below:
